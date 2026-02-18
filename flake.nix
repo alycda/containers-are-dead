bc8eb03f6db37490821883323d46b5102a892e0a
Alyssa Evans <alyda@me.com>
2026-02-18 15:49:14 +0000

    add wr to Nix devshell

--- flake.nix
+++ flake.nix
@@ -20,6 +20,16 @@
           inherit system overlays;
         };
         rustToolchain = with pkgs; rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
+        wr = pkgs.rustPlatform.buildRustPackage {
+          pname = "workshop-runner";
+          version = "0.2.5";
+          src = pkgs.fetchCrate {
+            pname = "workshop-runner";
+            version = "0.2.5";
+            hash = "sha256-OCLDGVctfJoMQ1LwZmGrOOPLnNB+FBhWJ909+DpqL5Q=";
+          };
+          cargoHash = "sha256-/Oj4B2W+fprOML1KdiU8fHkeGj1JXq8o0GlKxa46/64=";
+        };
       in
       {
         devShells.default = with pkgs; mkShell rec {
@@ -37,6 +47,9 @@
             cargo-component
 
             fermyon-spin
+
+            # workshop runner
+            wr
 
             # devtools
             mdbook
